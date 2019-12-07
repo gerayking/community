27cacf29caf9918ed6343c82fa72d8e1cd19206d
gerayking <919179287@qq.cin>
2019-12-07 21:32:55 +0800

    reserve token

--- src/main/java/life/gerayking/community/community/controller/HelloController.java
+++ src/main/java/life/gerayking/community/community/controller/HelloController.java
@@ -1,16 +1,38 @@
 package life.gerayking.community.community.controller;
 
+import life.gerayking.community.community.mapper.UserMapper;
+import life.gerayking.community.community.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 
 /*
 Creat by coderinker on 2019/12/2
 * */
 @Controller
-public class HelloController {
+@Component
+public class HelloController{
+    @Autowired
+    private UserMapper userMapper;
     @GetMapping("/")
-    public String index(){return "index";}
+    public String index(HttpServletRequest request){
+        Cookie[] cookies = request.getCookies();
+        for(Cookie cookie:cookies)
+        {
+            if(cookie.getName().equals("token")){
+                String token = cookie.getValue();
+                User user = userMapper.findBytoken(token);
+                if(user!=null){
+                    request.getSession().setAttribute("user",user);
+                }
+                break;
+            }
+        }
+
+        return "index";}
 
 }
